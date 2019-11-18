ca7be8a78c7b711d70ba82d5458f1811b949c1d4
ValentinaParietti <parietti.valentina@gmail.com>
2019-11-18 10:02:24 +0100

    prova

--- train.py
+++ train.py
@@ -34,7 +34,7 @@ if 1:
     tf_config     = {'rnd.np_random_seed': 1000}                                           # Options for tflib.init_tf().
 
     # Dataset.
-    desc += '-custom_datasets'; dataset = EasyDict(tfrecord_dir=r'datasets/custom_datasets', resolution=128); train.mirror_augment = False
+    desc += '-custom_datasets'; dataset = EasyDict(tfrecord_dir=r'custom_datasets', resolution=128); train.mirror_augment = False
     #desc += '-ffhq';     dataset = EasyDict(tfrecord_dir='ffhq');                 train.mirror_augment = True
     #desc += '-ffhq512';  dataset = EasyDict(tfrecord_dir='ffhq', resolution=512); train.mirror_augment = True
     #desc += '-ffhq256';  dataset = EasyDict(tfrecord_dir='ffhq', resolution=256); train.mirror_augment = True
