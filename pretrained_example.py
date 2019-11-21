6f72fe1077d3d6de611283d80b0b669069964696
ValentinaParietti <parietti.valentina@gmail.com>
2019-11-21 15:43:54 +0100

    update params

--- pretrained_example.py
+++ pretrained_example.py
@@ -20,7 +20,7 @@ def main():
     tflib.init_tf()
 
     # Load pre-trained network.
-    url = os.path.abspath("results/00006-sgan-custom_datasets-1gpu/network-snapshot-003845.pkl") # karras2019stylegan-ffhq-1024x1024.pkl
+    url = os.path.abspath("results/00006-sgan-custom_datasets-1gpu/network-snapshot-004005.pkl") # karras2019stylegan-ffhq-1024x1024.pkl
     with open(url, 'rb') as f:
         _G, _D, Gs = pickle.load(f)
         # _G = Instantaneous snapshot of the generator. Mainly useful for resuming a previous training run.
