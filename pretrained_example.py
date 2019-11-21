12722d0f14817df5e2a22dfa4287b96d6de5b7dd
ValentinaParietti <parietti.valentina@gmail.com>
2019-11-21 13:49:12 +0100

    update parameters to generate new images

--- pretrained_example.py
+++ pretrained_example.py
@@ -20,8 +20,8 @@ def main():
     tflib.init_tf()
 
     # Load pre-trained network.
-    url = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ' # karras2019stylegan-ffhq-1024x1024.pkl
-    with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
+    url = os.path.abspath("results/00006-sgan-custom_datasets-1gpu/network-snapshot-003845.pkl") # karras2019stylegan-ffhq-1024x1024.pkl
+    with open(url, 'rb') as f:
         _G, _D, Gs = pickle.load(f)
         # _G = Instantaneous snapshot of the generator. Mainly useful for resuming a previous training run.
         # _D = Instantaneous snapshot of the discriminator. Mainly useful for resuming a previous training run.
